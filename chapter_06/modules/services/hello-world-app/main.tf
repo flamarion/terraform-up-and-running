32a0445e041ba173592eb33c9bad8b212497b9dc
Flamarion Jorge <flamarion@hashicorp.com>
2020-04-22 13:03:57 +0200

    Releasing v0.0.5

--- chapter_06/modules/services/hello-world-app/main.tf
+++ chapter_06/modules/services/hello-world-app/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  # Require any 0.12.x version of Terraform
+  required_version = "~> 0.12"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  # Allow any 2.x version of the AWS provider
+  version = "~> 2.0"
+}
+
 data "aws_vpc" "default" {
   default = true
 }
